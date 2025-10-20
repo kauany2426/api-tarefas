d78c157d8e8dadeb95814f7d27b915fad3323710
kauany2426 <kauanyramostorres@gmail.com>
2025-10-20 15:48:47 -0300

    Criação do arquivo de configuração da tarefa com o banco de dados, cria-se a tabela tarefa no banco de dados.

--- src/main/java/com/kauany/apitarefas/model/Tarefa.java
+++ src/main/java/com/kauany/apitarefas/model/Tarefa.java
@@ -7,4 +7,74 @@ import java.time.LocalDate;
 @Table(name = "tarefa")
 
 public class Tarefa {
+
+    // Chave Primária
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(name = "data_entrega", nullable = false)
+    private LocalDate dataEntrega;
+
+    @Column(nullable = false)
+    private String responsavel;
+
+    // Construtor vazio (obrigatório para o JPA)
+    public Tarefa() {
+    }
+    // Construtor com parâmetros (facilita criar objetos)
+    public Tarefa(String nome, LocalDate dataEntrega, String responsavel) {
+        this.nome = nome;
+        this.dataEntrega = dataEntrega;
+        this.responsavel = responsavel;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataEntrega() {
+        return dataEntrega;
+    }
+
+    public void setDataEntrega(LocalDate dataEntrega) {
+        this.dataEntrega = dataEntrega;
+    }
+
+    public String getResponsavel() {
+        return responsavel;
+    }
+
+    public void setResponsavel(String responsavel) {
+        this.responsavel = responsavel;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
