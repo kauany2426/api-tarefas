0b4a7351d4adf5d5122de2b8ac1358e384951040
kauany2426 <kauanyramostorres@gmail.com>
2025-10-20 16:00:02 -0300

    Criação do arquivo em que instancia os dados por meio do Repository, usando a interface.

---
+++ src/main/java/com/kauany/apitarefas/repository/TarefaRepository.java
@@ -0,0 +1,12 @@
+package com.kauany.apitarefas.repository;
+
+import com.kauany.apitarefas.model.Tarefa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+
+}
+
+
